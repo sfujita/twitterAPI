3ccd3f34af517e7a49fd0732a80f85605baf870a
syun_alien <next.s.bdf@gmail.com>
2019-02-24 18:26:13 +0900

    テストも追加

---
+++ tweet.test/src/test/java/tweet/test/UserInfoMainTest.java
@@ -0,0 +1,14 @@
+package tweet.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserInfoMainTest {
+
+	@Test
+	public void test() {
+		fail("まだ実装されていません");
+	}
+
+}
